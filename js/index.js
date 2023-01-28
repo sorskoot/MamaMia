03ba4cf35c89a17e22373f73c0ea0c2716317775
Sorskoot <mental_c@hotmail.com>
2023-01-28 16:39:15 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/**
+ * This is the entry point of your standalone application.
+ *
+ * You should register the component you need here, e.g,
+ *
+ * ```
+ * import { MyComponent } from './my-component.js';
+ *
+ * WL.registerComponent(MyComponent);
+ * ```
+ */
+
+/* Register all default components. */
+import '@wonderlandengine/components';
