a33fa502f60b054676a0bad85edc71fe090df87a
Sorskoot <mental_c@hotmail.com>
2023-02-16 13:46:50 +0100

    fixed grabbable without grabPoint

--- js/interactions/grabbable.js
+++ js/interactions/grabbable.js
@@ -1,4 +1,4 @@
-import { Component, Object as Object3D} from '@wonderlandengine/api';
+import { Component, Object as Object3D } from '@wonderlandengine/api';
 import { quat, quat2, vec3 } from 'gl-matrix';
 import { getLocalTransform, reparentKeepTransform } from '../utils/helper';
 import { Grabber } from './grabber';
@@ -11,59 +11,60 @@ import { GrabPoint } from './grabPoint';
 export class Grabbable extends Component {
     static TypeName = 'grabbable';
     static Properties = {
-        grabPoint: {type: WL.Type.Object, default:null}
+        grabPoint: { type: WL.Type.Object, default: null }
     }
-    
+
     /** @type {Object3D} the original parent of the object */
     originalParent = null;
-    
-    /** @type {Grabber} the grabber that is currently holding this item */
-    grabbedBy;        
 
-    start(){
-        this.physx = this.object.getComponent('physx');              
+    /** @type {Grabber} the grabber that is currently holding this item */
+    grabbedBy;
+
+    start() {
+        this.physx = this.object.getComponent('physx');
         this.originalTransform = this.object.transformLocal;
     }
 
     /** @param {Grabber} grabber that is grabbing the item */
     grab(grabber) {
-        if(!this.originalParent){
+        if (!this.originalParent) {
             this.originalParent = this.object.parent;
         }
         this.grabbedBy = grabber;
 
-        if(this.physx){
+        if (this.physx) {
             this.physx.kinematic = true;
-        }
-        this.object.transformLocal = this.originalTransform ; 
+        }      
         reparentKeepTransform(this.object, grabber.object);
-        
+
         // but if there's a grab point, we should use that instead
-        /** @type {GrabPoint} */
-        let grabPoint = this.grabPoint.getComponent('grab-point');
-        if(grabPoint){
-            this.object.transformLocal = quat2.invert(quat2.create(), grabPoint.object.transformLocal);;
+        if (this.grabPoint) {
+            /** @type {GrabPoint} */
+            let grabPoint = this.grabPoint.getComponent('grab-point');
+            if (grabPoint) {
+                this.object.transformLocal = quat2.invert(quat2.create(), grabPoint.object.transformLocal);;
+            }
         }
 
         console.log(`grab by grabber ${grabber.object.name}`);
     }
 
     /** @param {Grabber} grabber that dropped the item */
-    drop(grabber){        
-        if(this.grabbedBy !== grabber){
+    drop(grabber) {
+        if (this.grabbedBy !== grabber) {
             return; //this item is not grabbed by this grabber
         }
-        this.grabbedBy = null;        
-               
+        this.grabbedBy = null;
+
         reparentKeepTransform(this.object, this.originalParent);
-        this.object.transformLocal = this.originalTransform ; 
-        
-        if(this.physx){
+        this.object.transformLocal = this.originalTransform;
+
+        if (this.physx) {
             this.physx.kinematic = false;
         }
     }
 
-    IsBeingHeld(){
+    IsBeingHeld() {
         return this.grabbedBy !== null;
     }
 };
